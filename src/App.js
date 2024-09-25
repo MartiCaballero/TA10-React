c5e883d4c11123df3fb7004e7debdaa99bdef12a
MartiCaballero <141785085+MartiCaballero@users.noreply.github.com>
2024-09-25 15:36:55 -0300

    ta10

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import AppT from './TA10.jsx';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/*Renderizar el componente MostrarTexto */}
+        <h1>Usuarios</h1>
+      <AppT />
+
+    </div>
+  );
+}
+
+export default App;
